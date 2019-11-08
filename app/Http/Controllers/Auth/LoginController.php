8dfed6e2b75b1f526bb78e05222fe13c643ad0c3
habibsentongo <habibsentongo@gmail.com>
2019-11-08 15:36:40 +0300

    create user login
    - provides route for user to Login
    - customize login form
    - add login with phone number functionality
    
    [Finishes #168950068]

--- app/Http/Controllers/Auth/LoginController.php
+++ app/Http/Controllers/Auth/LoginController.php
@@ -2,6 +2,7 @@
 
 namespace App\Http\Controllers\Auth;
 
+use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use Illuminate\Foundation\Auth\AuthenticatesUsers;
 
@@ -35,5 +36,19 @@ class LoginController extends Controller
     public function __construct()
     {
         $this->middleware('guest')->except('logout');
+    }
+
+    /**
+     * Get the needed authorization credentials from the request.
+     *
+     * @param  \Illuminate\Http\Request  $request
+     * @return array
+     */
+    protected function credentials(Request $request)
+    {
+        if(is_numeric($request->get('email'))){
+            return ['phonenumber'=>$request->get('email'),'password'=>$request->get('password')];
+        }
+        return $request->only($this->username(), 'password');
     }
 }
