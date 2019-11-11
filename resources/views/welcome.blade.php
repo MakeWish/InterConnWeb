12257b227ff1592681debd972ac326c90e418c28
Sentongo Habib <39903548+HabibSentongo@users.noreply.github.com>
2019-11-11 18:12:10 +0300

    enable user login (#8)
    - add a route for users to Login
    - customize login form
    - enable user login with either email or phone number
    
    [Finishes #168950068]

--- resources/views/welcome.blade.php
+++ resources/views/welcome.blade.php
@@ -6,7 +6,7 @@
     <!-- CSRF Token -->
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>{{ config('app.name', 'Interccon') }}</title>
+    <title>Interccon</title>
 
     <!-- Fonts -->
     <link rel="dns-prefetch" href="//fonts.gstatic.com">
