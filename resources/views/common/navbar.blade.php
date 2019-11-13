49816b8dc66ca9d0f8ccede2215e99e753acde16
Derrick Sekidde <kidricederek@gmail.com>
2019-11-13 14:00:43 +0300

    feat(Add navbar): Add default navbar - Add navbar for entire site - Add default site footer [finishes #169095445]

---
+++ resources/views/common/navbar.blade.php
@@ -0,0 +1,30 @@
+<header id="header">
+  
+  <div class="nav">
+    <input type="checkbox" id="nav-check">
+    <div class="nav-header">
+      <div class="nav-title">
+        INTERCCON
+      </div>
+    </div>
+    <div class="nav-btn">
+      <label for="nav-check">
+        <span></span>
+        <span></span>
+        <span></span>
+      </label>
+    </div>
+    
+    <div class="nav-links">
+      <a href="#about">About</a> 
+      <a href="#services">Services</a> 
+      <a href="/register">Register</a> 
+      <a href="/login">Login</a> 
+      <a href="#business">Make Order</a> 
+      <a href="#"><i class="fa fa-twitter"></i></a> 
+      <a href="#"><i class="fa fa-facebook"></i></a> 
+      <a href="#"><i class="fa fa-linkedin"></i></a> 
+      <a href="#"><i class="fa fa-envelope"></i></a>
+    </div>
+  </div>
+</header>
---
+++ resources/views/pages/default.blade.php
@@ -0,0 +1,13 @@
+<!Doctype html>
+<html lang="en">
+<head>
+  @include('common.head')
+</head>
+<body>
+  @include('common.navbar')
+  <div class="yowa-content">
+    <h1>Your content</h1>
+  </div>
+  @include('common.footer')
+</body>
+</html>
