382ae9e380c25ae29d77b0beef355b14cc333344
Sekidde Derrick <32802973+neelxie@users.noreply.github.com>
2019-11-20 09:42:47 +0300

    feat(Reset): Reset Password (#9)
    - Set up email credentials
    - Design relevant forms
    [finishes #168950160]

--- resources/views/auth/passwords/confirm.blade.php
+++ resources/views/auth/passwords/confirm.blade.php
@@ -5,9 +5,9 @@
     <div class="row justify-content-center">
         <div class="col-md-8">
             <div class="card">
-                <div class="card-header">{{ __('Confirm Password') }}</div>
+                <div class="card-header" style="background-color: #355723; color:white;">{{ __('Confirm Password') }}<img src="{{URL::asset('/fingerprint.png')}}" width="70" height="50" ></div>
 
-                <div class="card-body">
+                <div class="card-body" style="color:#d39c2f; border:1px #355723 solid;">
                     {{ __('Please confirm your password before continuing.') }}
 
                     <form method="POST" action="{{ route('password.confirm') }}">
@@ -17,7 +17,7 @@
                             <label for="password" class="col-md-4 col-form-label text-md-right">{{ __('Password') }}</label>
 
                             <div class="col-md-6">
-                                <input id="password" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
+                                <input id="password" style="border:1px #d39c2f solid;" type="password" class="form-control @error('password') is-invalid @enderror" name="password" required autocomplete="current-password">
 
                                 @error('password')
                                     <span class="invalid-feedback" role="alert">
@@ -29,7 +29,7 @@
 
                         <div class="form-group row mb-0">
                             <div class="col-md-8 offset-md-4">
-                                <button type="submit" class="btn btn-primary">
+                                <button type="submit" class="btn btn-primary" style="background-color:#355723; color:white;">
                                     {{ __('Confirm Password') }}
                                 </button>
 
